77753bd9066a4e9200898de743c35a2e5d0711d3
Jane <Jane@larrys-MacBook-Pro.local>
2025-08-10 08:59:40 +0800

    subscription

--- src/main/java/com/boyouquan/model/Subscription.java
+++ src/main/java/com/boyouquan/model/Subscription.java
@@ -9,13 +9,38 @@ public class Subscription {
 
     private String email;
     private Type type;
+    private String name;
     private Date subscribedAt;
     private Date unsubscribedAt;
     private Boolean unsubscribed = false;
 
     public enum Type {
-        MONTHLY_SELECTED,
-        ALL
+        MONTHLY_SELECTED("MONTHLY_SELECTED", "每月精选"),
+        ALL("ALL", "所有频道");
+
+        private final String key;
+        private final String value;
+
+        Type(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public static Type fromKey(String key) {
+            for (Type type : Type.values()) {
+                if (type.key.equals(key)) {
+                    return type;
+                }
+            }
+            return null;
+        }
+    }
+
+    public String getName() {
+        if (null == this.type) {
+            return null;
+        }
+        return type.value;
     }
 
 }

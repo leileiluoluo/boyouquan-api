2b212200c4f9fc0be8846ce49d882406fc21acc8
Jane <Jane@larrys-MacBook-Pro.local>
2025-09-17 11:40:51 +0800

    post images

---
+++ src/main/java/com/boyouquan/model/ImageDownloadResult.java
@@ -0,0 +1,21 @@
+package com.boyouquan.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Builder
+@Data
+public class ImageDownloadResult implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -1412983129684476982L;
+
+    private boolean success;
+    private String message;
+    private String imageType;
+    private String filePath;
+    private long totalBytes;
+
+}

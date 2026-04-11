87b4fdc4ef551e8ebf5fcaaa5cadccb902b0f2ed
Jane <Jane@larrys-MacBook-Pro.local>
2026-04-11 14:44:57 +0800

    large admin image

--- src/main/java/com/boyouquan/model/PostInfo.java
+++ src/main/java/com/boyouquan/model/PostInfo.java
@@ -20,6 +20,7 @@ public class PostInfo extends Post {
     private String blogAddress;
     private boolean blogStatusOk;
     private Long linkAccessCount;
+    private String blogAdminMediumImageURL;
     private String blogAdminLargeImageURL;
 
 }
